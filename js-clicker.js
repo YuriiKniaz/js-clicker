3832fb2bd3599fbd32cbccc91d99cb2ccb8dcace
YuriiKniaz <yuri.kniaz@gmail.com>
2023-07-16 13:51:08 +0300

    Cliker v1.0

--- js-clicker.js
+++ js-clicker.js
@@ -1,26 +1,41 @@
-const clicker = document.querySelector(".js-box");
-const mySpan = document.querySelector(".js-clicker");
+const button = document.getElementById("js-button");
+const counter = document.getElementById("counter");
+
+const timer = document.getElementById("timer");
+
+let sum = 0;
 
 
-let clicks = 0;
+function onClick(){
+  sum +=1;
 
-function onClick() {
-    clicks += 1;
+  counter.textContent = sum;
+  
+  
+};
 
-    mySpan.textContent = clicks;
+
+
+function startTimer() {
+  let timeLeft = 30;
+
+  const interval = setInterval(function () {
+    timeLeft -= 1;
+    timer.textContent = timeLeft;
+
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      timer.textContent = "Time is up!";
+      
+      button.disabled = true;
+
+      alert(`Congrats your score: ${counter.textContent}`)
+    }
+
+    return;
+},1000)
 
 }
 
-clicker.addEventListener('click', onClick);
-
-
-const myTimer = document.getElementById('timer');
-
-let seconds = 59;
-
-function timer(event) {
-     seconds -= 1;
-    myTimer.textContent = `:${seconds}`;
-}
-
-myTimer.addEventListener('click', timer, onClick);
+const click = button.addEventListener("click", onClick);
+const timerStart = button.addEventListener("click", startTimer, {once: true});
